9408971ccbd6d7773902ba588f85c6422ccaa9dd
manojkumar <manojkumar.manoharan@sap.com>
2022-01-08 12:10:44 +0530

    TC using T(N) = T(N) + N

--- src/main/java/dc/advance/resursion/reversion/recursion2/TowerOfHanoi.java
+++ src/main/java/dc/advance/resursion/reversion/recursion2/TowerOfHanoi.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
  * T[N^K] = 2^KT[N-K] + [2^K - 1]
  * N-K =0 // Base condition
  * K = N
+ * T[N] = 2^N + T(N-N) + 2^N-1
+ *      = 2^N + 2^N-1
  * T[N] = O(2^N)
  */
 public class TowerOfHanoi {
